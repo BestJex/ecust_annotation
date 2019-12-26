0fced4a5d13fffdba6dd09ded85a6d0eb0854b07
LiangMing <kyrieming@126.com>
2019-12-26 00:42:46 -0500

    add relation_entity_template

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-12-22 04:32:38
-@LastEditTime : 2019-12-25 03:02:13
+@LastEditTime : 2019-12-25 23:00:33
 @LastEditors  : Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /ecust_annotation/api/views.py
@@ -12,6 +12,7 @@ from api.models import Template
 from api.serializer import *
 from rest_framework import status
 from rest_framework.response import Response
+
 
 '''
 @description: 模板类型对应Serializer字典
@@ -102,33 +103,45 @@ class TemplateClassList(generics.ListCreateAPIView):
     def create(self,request, *args, **kwargs):
         #根据上传的data是否为list决定实例化serializer是many是否为True
         many = isinstance(self.request.data,list)
+
+        #根据urls上的信息完善serializer.data
+        data = self.enrich_serializer_data(many,request)
+
+        #如果是Response类型，说明里面出错，抛出
+        if isinstance(data,Response):
+            return data
+
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(data=request.data,many=many)
         serializer.is_valid(raise_exception=True)
-
-        #验证类型的一致性,如果一致性不正确，返回错误信息
-        if not self.validate_data_type(many,serializer):
-            content = {'type':'error','message':'this assembly is not supported by the template'}
-            return Response(content,status=status.HTTP_400_BAD_REQUEST)
-
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    #对于上传的数据类型一致性做验证，比如创建classification标签，对应的template的类型一定要是CLASSIFICATION
-    def validate_data_type(self,many,serializer):
-        #由于再serializer执行save()方法之前，不能直接访问serializer.data,因此访问serializer的validated_data
-        data = serializer.validated_data if many == True else [serializer.validated_data]
+    #将url上的template信息注入到data中，同时检验数据的一致性
+    #例如templateid为2的template为NER任务，则templateclass不应该为eventgroups
+    def enrich_serializer_data(self,many,request):
+        data = request.data if many == True else [request.data]
+        template_id = self.kwargs['pk']
         template_class = self.kwargs['templateclass']
-        
+
         for data_item in data:
-            #且validated_data会直接把外键链接的field直接变成template对象
-            template_type = data_item['template'].template_type
-            #如果存在某一个模板类型和template_class不对应，返回false；例如给一个NERtemplate创建CLASSIFICATION标签
-            if template_type not in  TemplateClassTemplateTypeDic[template_class]:
-                return False
-        return True
-                
+            #首先验证template的type和templateclass是否对应
+            try:
+                template_type = Template.objects.get(pk=template_id).template_type
+            except:
+                content = {'type':'error','message':'there is no template with pk = {}'.format(template_id)}
+                return Response(content,status=status.HTTP_400_BAD_REQUEST)
+
+            #再验证template_type和templateclass是否能够对应
+            if template_type not in TemplateClassTemplateTypeDic[template_class]:
+                content = {'type':'error','message':'this assembly is not supported by the template'}
+                return Response(content,status=status.HTTP_400_BAD_REQUEST)
+
+            #如果验证没有问题，data_item中添加template信息
+            data_item['template'] = template_id
+        
+        return data if many == True else data[0]
 '''
 @description: 根据实体组或事件组创建实体
 @param {type} 
@@ -153,29 +166,35 @@ class EntityTemplateList(generics.ListCreateAPIView):
     def create(self,request, *args, **kwargs):
         #根据上传的data是否为list决定实例化serializer是many是否为True
         many = isinstance(self.request.data,list)
+        #将实体组和事件组信息加入data中
+        data = self.enrich_validated_data(many,request)
+        
         serializer = self.serializer_class(data=request.data,many=many)
         serializer.is_valid(raise_exception=True)
-
-        if not self.validated_data_consistency(many,serializer):
-            content = {'type':'error','message':'the group in url and data is not consistent!'}
-            return Response(content,status=status.HTTP_400_BAD_REQUEST)
-
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     #创建实体时，验证url上的templateclass和上传数据是否一致,例如templateclass是entitygroups,则数据中entity_group_template不能为空，且event_group_template应该为空
-    def validated_data_consistency(self,many,serializer):
-        data = serializer.validated_data if many == True else [serializer.validated_data]
+    def enrich_validated_data(self,many,request):
+        data = request.data if many == True else [request.data]
         template_class = self.kwargs['templateclass']
+        template_class_id = self.kwargs['templateclassid']
+
+        event_group_template = template_class_id
+        entity_group_template = None
+
+        if template_class == 'entitygroups':
+            event_group_template, entity_group_template = entity_group_template, event_group_template
 
         for data_item in data:
-            if template_class == 'entitygroups' and data_item['entity_group_template'] is not None and data_item['event_group_template'] is None:
-                continue
-            if template_class == 'eventgroups' and data_item['event_group_template'] is not None and data_item['entity_group_template'] is None:
-                continue
-            return False
-        return True
+            #直接将数据加进去，不用验证是否存在，反正后面is_validated要再检验一次
+            data_item['entity_group_template'] = entity_group_template
+            data_item['event_group_template'] = event_group_template
+
+        return data
+                        
+
 
 '''
 @description: 建立实体和关系之间的联系，此时实体和关系已经全部建立完成
@@ -196,32 +215,56 @@ class RelationEntityList(generics.ListCreateAPIView):
     def create(self,request, *args, **kwargs):
         #根据上传的data是否为list决定实例化serializer是many是否为True
         many = isinstance(self.request.data,list)
-        serializer = self.serializer_class(data=request.data,many=many)
+
+        #返回修正后的data，如果修正过程中发现错误，data是一个Responce实例，应返回
+        data = self.enrich_validated_data(many,request)
+        if isinstance(data,Response):
+            return data
+
+        serializer = self.serializer_class(data=data,many=many)
         serializer.is_valid(raise_exception=True)
-
-        #检验关系两边实体所属的template是否为RE任务
-        if not self.validate_data_type(many,serializer):
-            content = {'type':'error','message':'this assembly is not supported by the template'}
-            return Response(content,status=status.HTTP_400_BAD_REQUEST)
-
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def validate_data_type(many,serializer):
-        data = serializer.validated_data if many == True else [serializer.validated_data]
+    #校验关系两端的实体是不是属于关系模板，同时补充relation信息
+    def enrich_validated_data(self,many,request):
+        data = request.data if many == True else [request.data] 
+        relation_id = self.kwargs['relationid']
 
         for data_item in data:
-            start_entity_template_type = self.get_template_type_by_entity(data_item['start_entity'])
-            end_entity_template_type = self.get_template_type_by_entity(data_item['end_entity'])
-            template_type = data_item['relation'].template.template_type
-
-            if not (start_entity_template_type == template_type and end_entity_template_type == template_type):
-                return False
-        return True                
+            start_entity_id = data_item['start_entity']
+            end_entity_id = data_item['end_entity']
+            #首先验证start_entity是否存在，并判断其所属模板类型是否为RE
+            start_entity = self.get_entity_tempalte_by_id(start_entity_id)
+            #如果404或实体所属模板不是RE返回异常
+            if isinstance(start_entity,Response):
+                return start_entity
+            end_entity = self.get_entity_tempalte_by_id(end_entity_id)
+            #如果404或实体所属模板不是RE返回异常
+            if isinstance(end_entity,Response):
+                return end_entity
+            #如果验证没有问题，data加上relation属性
+            data_item['relation'] = relation_id
+            
+        return data if many == True else data[0]
+        
+    #根据entity_template_id返回entity_template
+    def get_entity_tempalte_by_id(self,id):
+        try:
+            entity = Entity_template.objects.get(pk=id)
+            template_type = self.get_template_type_by_entity(entity)
+            #如果实体所属模板不是RE，返回错误
+            if template_type != 'RE':
+                content = {'type':'error','message':'could not add a relation on entity with pk={}, which is not an entity of relation template'.format(id)}
+                return Response(content,status=status.HTTP_400_BAD_REQUEST)  
+            return entity 
+        except:
+            content = {'type':'error','message':'there is no entity_template with pk = {}'.format(id)}
+            return Response(content,status=status.HTTP_400_BAD_REQUEST)        
 
     #输入一个entitytemplate实例，返回这个entity所属的template的type
-    def get_template_type_by_entity(entity):
+    def get_template_type_by_entity(self,entity):
         if entity.event_group_template is None:
             return entity.entity_group_template.template.template_type
         return entity.event_group_template.template.template_type        
